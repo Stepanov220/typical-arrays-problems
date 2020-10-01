5c7a761d06e8c622b989300637db3dd1e6c892b6
Stepanov220 <stepanov220@yahoo.com>
2020-10-02 00:22:48 +0300

    fix: fix index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  let noParam = 0;
+  let min = 0;
+  if (min = Math.min.apply(null, array)) {
+  return min;
+  } //else { (min = '' || min = ([])) 
+    //}
+    return noParam;
 }
 
 exports.max = function max (array) {
+  let max = 0;
+  if (max = Math.max(...array)){
+  return max;
+  }
   return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let arr = array;
+  let sum = 0;
+  for (var i = 0; i < arr.length; i++);
+  sum += arr[i];
+  let avg = sum / arr.length;
+  return avg;
 }
